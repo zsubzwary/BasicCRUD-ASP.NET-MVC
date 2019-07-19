79bc7998d7fc58e720c57d18b214db2191137893
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-07-19 09:11:52 +0500

    Created Student Model

---
+++ BasicFuncWebApp/Models/StudentModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BasicFuncWebApp.Models
+{
+    public class StudentModel
+    {
+        [Required]
+        [Display(Name = "First name")]
+        public string firstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string lastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+    }
+
+    public class Student
+    {
+        public int sid { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+    }
+}

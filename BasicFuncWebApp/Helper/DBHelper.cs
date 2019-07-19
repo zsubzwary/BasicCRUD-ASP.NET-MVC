dfd57770620e4fe8214b82a731bde55b69d667b1
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-07-19 09:31:10 +0500

    Added DBHelper class Also added function that givesBack connection String

---
+++ BasicFuncWebApp/Helper/DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicFuncWebApp.Helper
+{
+    public class DBHelper
+    {
+        public static String getConnectionString(String FileName = "BasicFuncWebAppDB.db")
+        {
+            string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+
+            string sqlLiteConnectionString = string.Format(
+              "data source=\"{0}\"",
+              System.IO.Path.Combine(baseFolder, FileName));
+            return sqlLiteConnectionString;
+        }
+    }
+}
